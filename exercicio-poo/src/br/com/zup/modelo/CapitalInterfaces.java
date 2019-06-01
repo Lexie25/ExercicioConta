6c0fa206673030f51b96ab47d8f93941ee343a17
Brenda <brenda.lima@zup.com.br>
2019-06-01 10:00:58 -0300

    Exercicio da transferencia bancaria

---
+++ exercicio-poo/src/br/com/zup/modelo/CapitalInterfaces.java
@@ -0,0 +1,7 @@
+package br.com.zup.modelo;
+
+public interface CapitalInterfaces {
+	public void depositar(double valor);
+	public void sacar(double valor);
+	public void tranferencia(double valor);
+}
--- exercicio-poo/src/br/com/zup/modelo/Conta.java
+++ exercicio-poo/src/br/com/zup/modelo/Conta.java
@@ -1,13 +1,13 @@
 package br.com.zup.modelo;
 
 public class Conta {
-	
+
 	// Atributos
 	private String numeroConta;
 	private String banco;
 	private String agencia;
 	private double saldo;
-	
+
 	// Construtor
 	public Conta(String numeroConta, String banco, String agencia,
 			double saldo) {
@@ -17,7 +17,7 @@ public class Conta {
 		this.agencia = agencia;
 		this.saldo = saldo;
 	}
-	
+
 	// Gets e Sets
 	public String getNumeroConta() {
 		return numeroConta;
@@ -50,17 +50,44 @@ public class Conta {
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
-	
+
 	// toString
 	@Override
 	public String toString() {
-		
+
 		String modelo = "";
 		modelo += "\n - Número Conta -> " + this.getNumeroConta() + "\n";
 		modelo += " -Banco ->" + this.getBanco() + "\n";
 		modelo += " -Agência -> " + this.getAgencia() + "\n";
 		modelo += " -Saldo -> " + this.getSaldo() + "\n";
-		
+
 		return modelo;
 	}
+	public void depositar(double valor) {
+		double totalValor =  this.getSaldo()+ valor;
+		this.setSaldo(totalValor);
+
+	}
+	public void sacar(double valor) { 
+		if (this.getSaldo() >= valor) { 
+			double valorTotal = this.getSaldo()-valor;
+			this.setSaldo(valorTotal);
+		}else { 
+			System.out.println("Você não pode sacar!");
+		}
+
+	}
+	public void saldoAtual(){
+		System.out.println("Seu saldo é:" + this.getSaldo());
+	}
+	public void transferencia(double valorTransferencia, conta conta) {
+		this.setSaldo(this.getSaldo()-valorTransferencia);
+		conta.setSaldo(conta.getSaldo()+ valorTransferencia);
+		if(valorTransferencia > this.getSaldo());
+		System.out.println("Saldo insuficiente");
+	} else{ 
+		System.out.println("Você conseguiu transferir com sucesso!"); 
+	}
 }
+
+
